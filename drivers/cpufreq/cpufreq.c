360944c948124267f9fece5d67578e3b9d5c6f68
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-11-20 00:44:12 +0100

    drivers: cpufreq: Import Xiaomi changes
    Change-Id: Ia1d5a0ee4d1070d367365bb64d6887c5b353eeda

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -2267,8 +2267,10 @@ static int cpufreq_set_policy(struct cpufreq_policy *policy,
 			CPUFREQ_ADJUST, new_policy);
 
 	/* adjust if necessary - hardware incompatibility */
+#ifndef CONFIG_MACH_XIAOMI
 	blocking_notifier_call_chain(&cpufreq_policy_notifier_list,
 			CPUFREQ_INCOMPATIBLE, new_policy);
+#endif
 
 	/*
 	 * verify the cpu speed can be set within this limit, which might be
